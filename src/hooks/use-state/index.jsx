4f36bfd4e7ff7f11b4a9dfb0367ef4f2d0ac7be5
virajkc1 <virajkc1@gmail.com>
2026-03-24 23:29:29 +0000

    React Hooks

--- src/hooks/use-state/index.jsx
+++ src/hooks/use-state/index.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
-
 export const StateExample = () => {
   const [count, setCount] = useState(0);
-
   const increaseCount = () => {
     setCount((prev) => prev + 1);
-
     console.log(count);
   };
-
   return (
     <div>
       <p> Count: {count} </p>
@@ -16,3 +12,14 @@ export const StateExample = () => {
     </div>
   );
 };
+/* 
+We need state for re rendering our UI for value that is changed
+
+state setter functions let you have a callback function, it allows you to have access to a previous value of a state and mutating it
+
+
+
+
+
+
+*/
