dbf39c5816a04670faf7ae8fc877da4dc6d7840f
virajkc1 <virajkc1@gmail.com>
2026-03-25 22:23:25 +0000

    useEffect Added

--- src/hooks/use-effect/index.jsx
+++ src/hooks/use-effect/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const EffectExample = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([]); //initial value is an empty list
   const [showPedro, setShowPedro] = useState(false);
 
   useEffect(() => {
@@ -9,6 +9,7 @@ export const EffectExample = () => {
       .then((response) => response.json())
       .then((json) => setData(json));
   }, []);
+  //you can make the useEffect run after every render or you can control it  based on a STATE VALUE
 
   useEffect(() => {
     console.log("Pedro");
@@ -19,10 +20,33 @@ export const EffectExample = () => {
       <button onClick={() => setShowPedro((prev) => !prev)}> Toggle </button>
       <h1> Posts</h1>
       <ul>
-        {data.map((item) => (
-          <li>{item.title} </li>
+        {data.map((item, index) => (
+          <li>
+            {item.title} {index}
+          </li>
         ))}
       </ul>
     </div>
   );
 };
+
+/*
+useEffect hook - one of the most powerful hooks in react
+
+Its used for fetching data from an API immediately when the page renders
+allows you to access systems outside of react environment
+
+Lets you run code after component renders
+
+If you wanted to run a function every time someone renders a page for the first time
+
+For fetching data - you can use a react query
+
+
+
+
+
+
+
+
+*/
--- src/hooks/use-state/index.jsx
+++ src/hooks/use-state/index.jsx
@@ -17,9 +17,9 @@ We need state for re rendering our UI for value that is changed
 
 state setter functions let you have a callback function, it allows you to have access to a previous value of a state and mutating it
 
+Using values of state causes re renders
 
-
-
-
+The console.log runs before the page decides to re render 
+React only re renders after the increaseCount function is run
 
 */
